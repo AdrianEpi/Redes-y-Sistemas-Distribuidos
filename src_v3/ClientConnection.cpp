644df8b46b896cd1fe0982096663ca97bbd5e3f1
Adrián Epifanio <44296284+AdrianEpi@users.noreply.github.com>
2019-05-18 17:33:54 +0100

    Update ClientConnection.cpp

--- src_v3/ClientConnection.cpp
+++ src_v3/ClientConnection.cpp
@@ -137,57 +137,147 @@ void ClientConnection::WaitForRequests() {
 	    fscanf(fd, "%s", arg);
 
 	    // fprintf(fd, "HOLA USUARIO: %s\n", arg);
-	      if (strcmp(arg, "jonas") == 0)
-		     fprintf(fd, "331 User name ok, need password.\n");
+	    if (strcmp(arg, "jonas") == 0)
+		    fprintf(fd, "331 User name ok, need password.\n");
 
-		 else
+		else
 		 	fprintf(fd, "530 Not logged in.\n");
 
       }
-      
+      //Comando SYST
       else if (COMMAND("PASS")) {
 		fscanf(fd, "%s", arg);
 
-	      if (strcmp(arg, "XXXX") == 0)
-		     fprintf(fd, "230 User logged in, proceed.\n");
+	    if (strcmp(arg, "XXXX") == 0)
+		    fprintf(fd, "230 User logged in, proceed.\n");
 
-		 else
+		else
 		 	fprintf(fd, "530 Not logged in.\n");
       }
-
+      //Comando SYST 
       else if (COMMAND("SYST")) {
-	   
+      	/**
+      	 *
+      	 * Con el comando #ifdef podemos detectar el sistema operativo. En el caso de __linux__ detectamos si es un linux, en caso de serlo se ejecutaría esa parte del codigo, en caso de no se pasaría el siguiente if.
+      	 *
+      	 */
+      	
+		#ifdef __linux__ 
+		    fprintf(fd, "215 UNIX system type.\n");
+		#elif _WIN32
+		    fprintf(fd, "215 MSDOS system type.\n");
+		#else
+		    fprintf(fd, "450 Error, system busy.\n");
+		#endif
       }
-      else if (COMMAND("TYPE")) {
-	  
-      }
+      //Comando TYPE
+      /**
+       *
+       * Se define el tipo de codificación del enlace de datos. Se reconocen los siguientes Tipos:
+		*TYPE A N TYPE ASCII NON PRINT. Se utiliza para transferir ficheros de texto. 'N'es opcional y puede no existir.
+		*TYPE A T TYPE ASCII TELNET. Se utiliza para transferir ficheros de texto. (Nosoportado).
+		*TYPE A C TYPE ASCII CARRIAGE. Se utiliza para transferir ficheros de texto. (No soportado).
+		*TYPE I TYPE IMAGE. Se utiliza para transferir ficheros binarios.
+		*TYPE E TYPE EBCDIC. (No soportado).
+		*TYPE L 8 TYPE LOCAL. Solo se soporta LOCAL 8 = IMAGE.
+       *
+       */
+      
+    else if (COMMAND("TYPE")) {
+    	fscanf(fd, "%s", arg);
+	  	if (strcmp(arg, "A") == 0)
+	  		fprintf(fd, "200 TYPE is set to ASCII NON PRINT.\n");
+	  	
+	  	else if (strcmp(arg, "A N") == 0)
+	  		fprintf(fd, "200 TYPE is set to ASCII NON PRINT.\n");
+	  	
+	  	else if (strcmp(arg, "A T") == 0)
+	  		fprintf(fd, "200 TYPE is set to ASCII TELNET.\n");
+
+	  	else if (strcmp(arg, "A C") == 0)
+	  		fprintf(fd, "504 TYPE ASCII CARRIAGE not supported.\n");
+
+	  	else if (strcmp(arg, "I") == 0)
+	  		fprintf(fd, "200 TYPE is set to IMAGE.\n");
+
+	  	else if (strcmp(arg, "E") == 0)
+	  		fprintf(fd, "504 TYPE EBCDIC not supported.\n");
+
+	  	else if (strcmp(arg, "L 8") == 0)
+	  		fprintf(fd, "200 TYPE is set to LOCAL 8.\n");
+
+	  	else if (strcmp(arg, "") == 0)
+	  		fprintf(fd, "450 Error, system busy.\n");
+
+	  	else
+	  		fprintf(fd, "501 TYPE argument error.\n");
+
+    }
 
       //Formas de abrir el socket
-      else if (COMMAND("PORT")) {//modo activo
+      //Comando PORT para abrir el socket en modo activo
+      else if (COMMAND("PORT")) {
       	//PORT h0, h1, h2, h3, p0, p1
       	int h0, h1, h2, h3;
       	int p0, p1;
-	  	//fscanf(fd, "d", "d", "d", "d", "d", "d", &h0, &h1, &h2, &h3, &p0, &p1);
-	  	//uint32_t address = h0 << 24 | h1 << 16 | h2 << 8 | h3;
-	  	//uint32_t port = p0 << 8 | p1;
+	  	fscanf(fd, "%d,%d,%d,%d,%d,%d", &h0, &h1, &h2, &h3, &p0, &p1);
+	  	uint32_t address = h0 << 24 | h1 << 16 | h2 << 8 | h3;
+	  	uint32_t port = p0 << 8 | p1;
 
       }
       else if (COMMAND("PASV")) { //modo pasivo
 	  
       }
       /////////////////////////////////////////
+      //Cambia el directorio de trabajo al directorio que indica el parámetro. Ahora solo sirve para poder tratar los sistemas de ficheros como directorios, también admite '..' o '/' para subir al directorio raíz.
+    else if (COMMAND("CWD")) {
+    	fscanf(fd, "%s", arg);
+		if (strcmp(arg, "/") == 0 || strcmp(arg, "..") == 0)
+		{
+			//system("cd /");
+			fprintf(fd, "200 CWD root dir successful.\n");  
+		}
 
-      else if (COMMAND("CWD")) {
-	   
-      }
-      else if (COMMAND("PWD")) {
-	   
-      }        
-      else if (COMMAND("QUIT")) {
-	 
-      }
+		else if (strcmp(arg, "") == 0 )
+		{
+			//system("cd /");
+			fprintf(fd, "501 No pathname defined.\n");  
+		}
 
-      //PAra transmitir ficheros
+		else if (strcmp(arg, "/") != 0 && strcmp(arg, "..") != 0 && strcmp(arg, "") != 0)
+		{
+			//system("cd %s", arg);
+			fprintf(fd, "200 CWD current dir successful.\n");  
+		}
+
+		else
+		{
+			fprintf(fd, "450 Error, system busy.\n");  
+		}
+    }
+    
+    //Envía el nombre del directorio de trabajo. El servidor FTP solo se encuentra implementado para funcionar en el directorio raíz de los sistemas de ficheros. Además, indica el sistema de ficheros utilizado.
+    else if (COMMAND("PWD")) {
+	   char arg2 = system("pwd");
+	   if (arg2 == '/' == 0 )
+		{
+			//system("cd /");
+			fprintf(fd, "257 %d is current directory\n", arg2);  
+		}
+		else if(COMMAND("PASSWORD"))
+			fprintf(fd, "530 Access denied, not logged in.\n"); 
+			
+		else 
+			fprintf(fd, "450 Error, system busy.\n"); 
+
+    }        
+    
+    //Comando Quit, sale del sistema y cierra la conexión con el servidor
+    else if (COMMAND("QUIT")) {
+    	fprintf(fd, "221 Goodbye.\n"); 
+    }
+
+      //Para transmitir ficheros
       else if (COMMAND("RETR")) {
 	   
       }
